652c89085f530faf7ee227540443d6208928f02d
Intertlr <d.papchenko@gmail.com>
2017-12-05 23:55:46 +0300

    add circle

--- js/Game.js
+++ js/Game.js
@@ -1,15 +1,12 @@
-
-var map = new Map("ground");
-map.sayName();
-
 function preload() {
 
 }
 
 function setup() {
-
+    var map = new Map("ground");
+    map.sayName();
 }
 
 function draw() {
-
+    ellipse(50, 50, 50, 50);
 }
