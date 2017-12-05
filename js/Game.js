c9af20aafb87aa9eb045a3077ae370d375c763fe
Intertlr <d.papchenko@gmail.com>
2017-12-05 21:46:54 +0300

    add tilemap

--- js/Game.js
+++ js/Game.js
@@ -2,16 +2,25 @@ const game = new Phaser.Game(
     window.innerWidth,
     window.innerHeight,
     Phaser.AUTO,
-    'test123',
+    '',
     { preload: preload, create: create, update: update }
 );
 
 function preload() {
-
+    game.load.tilemap('map', 'map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'terrainSet.png');
 }
+
+var map;
+var layer;
 
 function create() {
     game.stage.backgroundColor = "#2e2e2e";
+
+    map = game.add.tilemap('map');
+    map.addTilesetImage('terrainSet', 'tiles');
+    layer = map.createLayer('World1');
+    layer.resizeWorld();
 }
 
 function update() {
