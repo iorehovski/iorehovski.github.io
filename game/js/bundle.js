1bbbb6729b541df7ff8a5165b5d164a6385f6eaa
iorehovski <iorehovski@gmail.com>
2017-12-20 16:34:42 +0300

    fix

--- game/js/bundle.js
+++ game/js/bundle.js
@@ -1,3 +1,187 @@
+let map;
+let player = {};
+let techData;
+let enemies;
+
+let jsonMap;
+let jsonBunkerMap;
+let jsonItems;
+let jsonWeapon;
+
+let images;
+let blood;
+let spritesBlood; 
+let playerSprites = [];
+let gunSpriteSheet;
+let zimbieSprites = [];
+
+let itemsGenerator;
+let itemsSpriteSheet;
+
+let sounds = {};
+let soundsQueue = [];
+
+let things = [];    //things as medicine kit, ammo, weapons, etc. on the map
+
+let gameOver = false;
+let gameIsPaused = false;
+let gameIsWon = false;
+let keyIsPressed = false;
+
+let font;
+
+let wavesEnemies;
+
+let fpsValue;
+
+function preload() {
+    jsonMap = loadJSON(MAP_JSON_PATH);
+    jsonBunkerMap = loadJSON(BUNKER_JSON_PATH);
+    jsonItems = loadJSON(ITEMS_JSON_PATH);
+    jsonWeapon = loadJSON(WEAPON_JSON_PATH);
+
+    font = loadFont('../game/fonts/SquadaOne-Regular.ttf');
+    
+    images = loadImage('../game/img/terrainSet.png');
+    spritesBlood = loadImage('../game/img/blood_spot.png');
+    gunSpriteSheet = loadImage('../game/img/gunSpriteSheet.png');
+    itemsSpriteSheet = loadImage('../game/img/itemsSheet.png');
+
+    sounds.glock17 = loadSound('../game/audio/gun/pistol_shot.wav');
+    sounds.glock17Reload = loadSound('../game/audio/gun/pistol_reload.mp3');
+    sounds.ak47 = loadSound('../game/audio/gun/ak47_shot.mp3');
+    sounds.ak47Reload = loadSound('../game/audio/gun/ak47_reload.mp3');
+    sounds.m4a1 = loadSound('../game/audio/gun/m4a1_shot.mp3');
+    sounds.m4a1Reload = loadSound('../game/audio/gun/m4a1_reload.mp3');
+    sounds.awp = loadSound('../game/audio/gun/awp_shot.mp3');
+    sounds.awpReload = loadSound('../game/audio/gun/awp_reload.mp3');
+
+    sounds.music = {};
+    
+    //sounds.music.track1 = loadSound('../game/audio/Resident_Evil_movie_soundtrack_2008.mp3');
+    //sounds.music.track2 = loadSound('../game/audio/Resident_Evil_Corp_Umbrella.mp3');
+
+    zimbieSprites[0] = [];
+    for(let i = 0; i < 17; i++) {
+        zimbieSprites[0][i] = loadImage('../game/img/enemy/zombieNormal/skeleton-move_' + i + '.png');
+    }
+    zimbieSprites[1] = [];
+    for(let i = 0; i < 9; i++) {
+        zimbieSprites[1][i] = loadImage('../game/img/enemy/zombieNormal/skeleton-attack_' + i + '.png');
+    }
+
+    playerSprites[0] = loadImage('../game/img/player/survivor-glock.png');
+    playerSprites[1] = loadImage('../game/img/player/survivor-ak47.png');
+    playerSprites[2] = loadImage('../game/img/player/survivor-m4a1.png');
+    playerSprites[3] = loadImage('../game/img/player/survivor-awp.png');
+
+}
+
+function setup() {
+    enemies = [];
+    frameRate(60);
+    createCanvas(WIN_WIDTH, WIN_HEIGHT);
+
+    player = new Player(ENTITY_DIAMETR / 2, {'x': 2500, 'y': 1700}, playerSprites);
+    map = new Map({
+        'x': 0,
+        'y': 0
+    });
+
+    map.imagesSet = images;
+    map.createMap(jsonMap);
+
+
+    itemsGenerator = new Generation(map.map, jsonItems, jsonWeapon, player, enemies, zimbieSprites);
+    itemsGenerator.createGenerationArea(map.map);
+    setInterval(function() {
+        itemsGenerator.findEnemiesOnScreen(enemies, player.pos);
+    }.bind(this), 2000);
+
+    wavesEnemies = new WaveEnemies();
+    wavesEnemies.launchNewWaves();
+
+    blood = new Blood();
+
+    background(BGCOLOR_GRAY);
+
+    sounds.glock17.setVolume(0.3);
+    sounds.glock17Reload.setVolume(0.3);
+    sounds.ak47.setVolume(0.3);
+    sounds.ak47Reload.setVolume(0.3);
+    sounds.m4a1.setVolume(0.3);
+    sounds.m4a1Reload.setVolume(0.3);
+    sounds.awp.setVolume(0.3);
+    sounds.awpReload.setVolume(0.3);
+
+    setStandartPlayerKit();
+
+    //set fps update time
+    setInterval(function() {
+        fpsValue = frameRate().toFixed(0);
+    }.bind(this), 500);
+
+    itemsGenerator.addWeapon(200, 200, 1);
+    itemsGenerator.addWeapon(300, 200, 2);
+    itemsGenerator.addWeapon(400, 200, 3);
+    itemsGenerator.addThing(500, 200, 0);
+    itemsGenerator.addThing(600, 200, 1);
+
+}
+
+function draw() {
+    if(gameOver) {
+        gameIsPaused = true;
+        $('.gameOverMenu').show();
+        // $('.gameScore').text('score:' + player.score.value);
+    }
+    if(gameIsPaused) {
+        return;
+    }
+
+    camera(player.pos.x - WIN_WIDTH_HALF, player.pos.y - WIN_HEIGHT_HALF);
+
+    map.update(player.pos);
+
+    blood.update();
+
+    itemsGenerator.generateItem();
+    itemsGenerator.updateItems();
+
+    itemsGenerator.generateEnemy();
+    itemsGenerator.updateEnemies(map.map, player);
+
+    wavesEnemies.update(player.pos);
+
+    printTechData( {
+        'xPlayer': player.pos.x, 
+        'yPlayer': player.pos.y,
+        'frameRate': fpsValue,
+        'enemiesNum': enemies.length
+    });
+
+    player.update(map);
+}
+
+function setStandartPlayerKit() {
+    //set standart inventory of player
+    //glock17
+    const item = JSON.parse(JSON.stringify(jsonWeapon.contents[0]));
+    item.pos.x = 0;
+    item.pos.y = 0;
+    player.putThingInInventory(new Weapon(item));
+    player.currentWeaponInHand = player.inventory.getItem(0);
+
+    itemsGenerator.generatedWeaponNames.push(item.name);
+}
+
+function keyPressed() {
+    keyIsPressed = true;
+}
+
+function keyReleased() {
+    keyIsPressed = false;
+}
 class Animation {
     constructor(imagesSet) {
         this.imagesSet = imagesSet;
@@ -68,7 +252,6 @@ class Animation {
         }
     }
 }
-
 class Bar {
     constructor() {
         this.value = 150;
@@ -118,7 +301,6 @@ class ColdBar extends Bar {
         this.strokeCol = '#0f79af';
     }
 }
-
 class BloodItem {
     constructor(posX, posY) {
         this.x = posX;
@@ -160,7 +342,6 @@ class Blood {
         }
     }
 }
-
 class Bullet {
     constructor() {
         this.bulletsList = [];
@@ -404,7 +585,6 @@ const BUNKER_JSON_PATH = '../game/js/bunkerJSON.json';
 const INVENTORY_THING_SIZE = 100;
 const ITEM_SIZE = 60;
 
-
 class Enemy {
     constructor(x, y, r, spriteSet) {
         this.r = r;
@@ -570,191 +750,6 @@ $('.resumeFinishBtn').click(function(){
     gameIsPaused = false;
     gameIsWon = false;
 });
-
-let map;
-let player = {};
-let techData;
-let enemies;
-
-let jsonMap;
-let jsonBunkerMap;
-let jsonItems;
-let jsonWeapon;
-
-let images;
-let blood;
-let spritesBlood; 
-let playerSprites = [];
-let gunSpriteSheet;
-let zimbieSprites = [];
-
-let itemsGenerator;
-let itemsSpriteSheet;
-
-let sounds = {};
-let soundsQueue = [];
-
-let things = [];    //things as medicine kit, ammo, weapons, etc. on the map
-
-let gameOver = false;
-let gameIsPaused = false;
-let gameIsWon = false;
-let keyIsPressed = false;
-
-let font;
-
-let wavesEnemies;
-
-let fpsValue;
-
-function preload() {
-    jsonMap = loadJSON(MAP_JSON_PATH);
-    jsonBunkerMap = loadJSON(BUNKER_JSON_PATH);
-    jsonItems = loadJSON(ITEMS_JSON_PATH);
-    jsonWeapon = loadJSON(WEAPON_JSON_PATH);
-
-    font = loadFont('../game/fonts/SquadaOne-Regular.ttf');
-    
-    images = loadImage('../game/img/terrainSet.png');
-    spritesBlood = loadImage('../game/img/blood_spot.png');
-    gunSpriteSheet = loadImage('../game/img/gunSpriteSheet.png');
-    itemsSpriteSheet = loadImage('../game/img/itemsSheet.png');
-
-    sounds.glock17 = loadSound('../game/audio/gun/pistol_shot.wav');
-    sounds.glock17Reload = loadSound('../game/audio/gun/pistol_reload.mp3');
-    sounds.ak47 = loadSound('../game/audio/gun/ak47_shot.mp3');
-    sounds.ak47Reload = loadSound('../game/audio/gun/ak47_reload.mp3');
-    sounds.m4a1 = loadSound('../game/audio/gun/m4a1_shot.mp3');
-    sounds.m4a1Reload = loadSound('../game/audio/gun/m4a1_reload.mp3');
-    sounds.awp = loadSound('../game/audio/gun/awp_shot.mp3');
-    sounds.awpReload = loadSound('../game/audio/gun/awp_reload.mp3');
-
-    sounds.music = {};
-    
-    //sounds.music.track1 = loadSound('../audio/Resident_Evil_movie_soundtrack_2008.mp3');
-    //sounds.music.track2 = loadSound('../audio/Resident_Evil_Corp_Umbrella.mp3');
-
-    zimbieSprites[0] = [];
-    for(let i = 0; i < 17; i++) {
-        zimbieSprites[0][i] = loadImage('../game/img/enemy/zombieNormal/skeleton-move_' + i + '.png');
-    }
-    zimbieSprites[1] = [];
-    for(let i = 0; i < 9; i++) {
-        zimbieSprites[1][i] = loadImage('../game/img/enemy/zombieNormal/skeleton-attack_' + i + '.png');
-    }
-
-    playerSprites[0] = loadImage('../game/img/player/survivor-glock.png');
-    playerSprites[1] = loadImage('../game/img/player/survivor-ak47.png');
-    playerSprites[2] = loadImage('../game/img/player/survivor-m4a1.png');
-    playerSprites[3] = loadImage('../game/img/player/survivor-awp.png');
-
-}
-
-function setup() {
-    enemies = [];
-    frameRate(60);
-    createCanvas(WIN_WIDTH, WIN_HEIGHT);
-
-    player = new Player(ENTITY_DIAMETR / 2, {'x': 2500, 'y': 1700}, playerSprites);
-    map = new Map({
-        'x': 0,
-        'y': 0
-    });
-
-    map.imagesSet = images;
-    map.createMap(jsonMap);
-
-
-    itemsGenerator = new Generation(map.map, jsonItems, jsonWeapon, player, enemies, zimbieSprites);
-    itemsGenerator.createGenerationArea(map.map);
-    setInterval(function() {
-        itemsGenerator.findEnemiesOnScreen(enemies, player.pos);
-    }.bind(this), 2000);
-
-    wavesEnemies = new WaveEnemies();
-    wavesEnemies.launchNewWaves();
-
-    blood = new Blood();
-
-    background(BGCOLOR_GRAY);
-
-    sounds.glock17.setVolume(0.3);
-    sounds.glock17Reload.setVolume(0.3);
-    sounds.ak47.setVolume(0.3);
-    sounds.ak47Reload.setVolume(0.3);
-    sounds.m4a1.setVolume(0.3);
-    sounds.m4a1Reload.setVolume(0.3);
-    sounds.awp.setVolume(0.3);
-    sounds.awpReload.setVolume(0.3);
-
-    setStandartPlayerKit();
-
-    //set fps update time
-    setInterval(function() {
-        fpsValue = frameRate().toFixed(0);
-    }.bind(this), 500);
-
-    itemsGenerator.addWeapon(200, 200, 1);
-    itemsGenerator.addWeapon(300, 200, 2);
-    itemsGenerator.addWeapon(400, 200, 3);
-    itemsGenerator.addThing(500, 200, 0);
-    itemsGenerator.addThing(600, 200, 1);
-
-}
-
-function draw() {
-    if(gameOver) {
-        gameIsPaused = true;
-        $('.gameOverMenu').show();
-        // $('.gameScore').text('score:' + player.score.value);
-    }
-    if(gameIsPaused) {
-        return;
-    }
-
-    camera(player.pos.x - WIN_WIDTH_HALF, player.pos.y - WIN_HEIGHT_HALF);
-
-    map.update(player.pos);
-
-    blood.update();
-
-    itemsGenerator.generateItem();
-    itemsGenerator.updateItems();
-
-    itemsGenerator.generateEnemy();
-    itemsGenerator.updateEnemies(map.map, player);
-
-    wavesEnemies.update(player.pos);
-
-    printTechData( {
-        'xPlayer': player.pos.x, 
-        'yPlayer': player.pos.y,
-        'frameRate': fpsValue,
-        'enemiesNum': enemies.length
-    });
-
-    player.update(map);
-}
-
-function setStandartPlayerKit() {
-    //set standart inventory of player
-    //glock17
-    const item = JSON.parse(JSON.stringify(jsonWeapon.contents[0]));
-    item.pos.x = 0;
-    item.pos.y = 0;
-    player.putThingInInventory(new Weapon(item));
-    player.currentWeaponInHand = player.inventory.getItem(0);
-
-    itemsGenerator.generatedWeaponNames.push(item.name);
-}
-
-function keyPressed() {
-    keyIsPressed = true;
-}
-
-function keyReleased() {
-    keyIsPressed = false;
-}
 
 function printTechData(objData) {
     fill(255);
@@ -1047,6 +1042,7 @@ class Generation {
     }
 }
 
+
 class Inventory {
     constructor() {
         this.inventoryThings = [];
@@ -1274,6 +1270,7 @@ class Map {
         }
     }
 }
+
 
 class Player {
     constructor(radius, windowDimentions, playerSprites) {
@@ -1559,6 +1556,7 @@ class Player {
         }
     }
 }
+
 class Score {
     constructor() {
         this.value = 0;
@@ -1581,6 +1579,7 @@ class Score {
         pop();
     }
 }
+
 class Thing {
     constructor(kit) {
         this.name = kit.name;
